575ff977b37ebfb832855d04e999927d86dbe6d8
Vladimir Tiurin <bfg.tyurin@gmail.com>
2018-09-29 22:47:06 +0300

    add addToCart() localStorage stub

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,7 @@
-console.log('hihih')
+const CART = "cart"
+addToCart = () => {
+  const current = localStorage.getItem(CART) || 0;
+  const updatedCart = parseInt(current) + 1;
+  localStorage.setItem(CART, updatedCart);
+  console.log(localStorage.getItem(CART));
+}
