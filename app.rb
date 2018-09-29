0be87ff88d23306eaf2270b904748a5febfacbef
Vladimir Tiurin <bfg.tyurin@gmail.com>
2018-09-29 23:39:00 +0300

    add Product entity, create products migration and Rakefile

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/reloader' if development?
 
+set :database, 'sqlite3:pizzashop.db'
+
+class Product < ActiveRecord::Base
+end
+
 get '/' do
   erb :index
 end
