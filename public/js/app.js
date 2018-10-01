0f9c3e45de11aa083f693a8c6a2ceddefa9c6894
Vladimir Tiurin <bfg.tyurin@gmail.com>
2018-10-02 01:02:16 +0300

    add basic localStorage cart for products

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,15 @@
 const CART = "cart"
-addToCart = () => {
-  const current = localStorage.getItem(CART) || 0;
-  const updatedCart = parseInt(current) + 1;
-  localStorage.setItem(CART, updatedCart);
+addToCart = (product_id) => {
+  const currentCart = JSON.parse(localStorage.getItem(CART)) || { products: [] };
+
+  let currentProduct = currentCart.products.find(product => product.id == product_id);
+  if(!currentProduct) {
+    const newProduct = { id: product_id, amount: 1 };
+    currentCart.products.push(newProduct);
+  } else {
+    currentProduct.amount += 1;
+  }
+
+  localStorage.setItem(CART, JSON.stringify(currentCart));
   console.log(localStorage.getItem(CART));
 }
