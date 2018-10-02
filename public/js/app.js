b6d1f011f4f18f8e71b1a2ff83924f909dfe9651
Vladimir Tiurin <bfg.tyurin@gmail.com>
2018-10-02 07:38:08 +0300

    add product counter on buttons

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,27 @@
-const CART = "cart"
-addToCart = (product_id) => {
-  const currentCart = JSON.parse(localStorage.getItem(CART)) || { products: [] };
+$(() => {
+  const cart = JSON.parse(localStorage.getItem('cart'));
+  if(!cart) {
+    localStorage.setItem('cart', JSON.stringify({ items: [] }));
+  }
+});
 
-  let currentProduct = currentCart.products.find(product => product.id == product_id);
-  if(!currentProduct) {
-    const newProduct = { id: product_id, amount: 1 };
-    currentCart.products.push(newProduct);
+addToCart = (product_id) => {
+  const currentCart = JSON.parse(localStorage.getItem('cart'));
+  const items = currentCart.items;
+
+  let index = items.findIndex(item => item.id === product_id);
+  if (index === -1) {
+    const newItem = { id: product_id, amount: 1 };
+    index = items.push(newItem) - 1;
   } else {
-    currentProduct.amount += 1;
+    items[index].amount += 1;
   }
 
-  localStorage.setItem(CART, JSON.stringify(currentCart));
-  console.log(localStorage.getItem(CART));
+  localStorage.setItem('cart', JSON.stringify(currentCart));
+  updateQTY(product_id, items[index].amount);
+}
+
+updateQTY = (id, qty) => {
+  domElement = document.getElementsByClassName('product_qty_' + id)[0];
+  domElement.innerHTML = qty;
 }
