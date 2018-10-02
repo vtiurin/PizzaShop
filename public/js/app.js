5facc08fa32c6278f7651b7f011e3fb67c98fa9a
Vladimir Tiurin <bfg.tyurin@gmail.com>
2018-10-02 17:57:45 +0300

    add cart nav button and update qty on it

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,21 @@
+const CART = 'cart';
 $(() => {
-  const cart = JSON.parse(localStorage.getItem('cart'));
+  const cart = JSON.parse(localStorage.getItem(CART));
   if(!cart) {
-    localStorage.setItem('cart', JSON.stringify({ items: [] }));
+    localStorage.setItem(CART, JSON.stringify({ items: [] }));
   } else {
     cart.items.map((item) => {
       const id = item.id;
       if (id) {
         updateQTY(id, item.amount);
+        updateCartButtonQTY(cart.items);
       }
     });
   }
 });
 
 addToCart = (product_id) => {
-  const currentCart = JSON.parse(localStorage.getItem('cart'));
+  const currentCart = JSON.parse(localStorage.getItem(CART));
   const items = currentCart.items;
 
   let index = items.findIndex(item => item.id === product_id);
@@ -24,11 +26,18 @@ addToCart = (product_id) => {
     items[index].amount += 1;
   }
 
-  localStorage.setItem('cart', JSON.stringify(currentCart));
+  localStorage.setItem(CART, JSON.stringify(currentCart));
   updateQTY(product_id, items[index].amount);
+  updateCartButtonQTY(items);
 }
 
 updateQTY = (id, qty) => {
   domElement = document.getElementsByClassName('product_qty_' + id)[0];
+  domElement.innerHTML = qty;
+}
+
+updateCartButtonQTY = (items) => {
+  domElement = document.getElementsByClassName('cart_button')[0];
+  const qty = items.reduce((acc, item) => acc += item.amount, 0);
   domElement.innerHTML = qty;
 }
